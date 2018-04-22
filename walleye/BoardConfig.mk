35f8bae7ba02b45c282bdf5bf08362442698bdad
calebcabob <calphonic@gmail.com>
2018-04-22 12:30:21 -0400

    Prep for Snoke kernel inline
    Also fix path to blobs Config
    Signed-off-by: calebcabob <calphonic@gmail.com>

--- walleye/BoardConfig.mk
+++ walleye/BoardConfig.mk
@@ -17,30 +17,21 @@
 TARGET_BOOTLOADER_BOARD_NAME := walleye
 DEFAULT_LOW_PERSISTENCE_MODE_BRIGHTNESS := 0x00000056
 
-BOARD_KERNEL_CMDLINE += console=ttyMSM0,115200,n8 earlycon=msm_serial_dm,0xc1b0000
-TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CLANG_VERSION := clang-stable
-TARGET_KERNEL_SOURCE := kernel/google/wahoo
-TARGET_KERNEL_CONFIG := custom_defconfig
-TARGET_KERNEL_ARCH := arm64
-BOARD_KERNEL_IMAGE_NAME := Image.lz4-dtb
-
 include device/google/wahoo/BoardConfig.mk
--include vendor/google_devices/walleye/proprietary/BoardConfigVendor.mk
+-include vendor/google/walleye/BoardConfigVendor.mk
 
+# Inline kernel building
+TARGET_KERNEL_SOURCE := kernel/google/wahoo
+BOARD_KERNEL_IMAGE_NAME := Image.lz4-dtb
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_NEEDS_DTBOIMAGE := true
+TARGET_KERNEL_CONFIG := snoke_defconfig
 BOARD_BOOTIMAGE_PARTITION_SIZE := 33554432
 
-#sepolicy common to walleye/walleye
+#sepolicy common to muskie/walleye
 BOARD_SEPOLICY_DIRS += device/google/walleye/sepolicy
 
 # Testing related defines
 BOARD_PERFSETUP_SCRIPT := platform_testing/scripts/perf-setup/wahoo-setup.sh
 
 BOARD_LISA_TARGET_SCRIPTS := device/google/wahoo/lisa/
-
-# VTS DTBO Verification. This kernel cmdline parameter should be added by the bootloader
-# for all future devices.
-BOARD_KERNEL_CMDLINE += androidboot.dtbo_idx=3
-
-# Enable for cleam flash
-BOARD_USES_SYSTEM_OTHER_ODEX := true
