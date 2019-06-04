a0ee72c2de7faac470ab9b8cc12f78479db09b4f
buffal0b1ll <billblandford@gmail.com>
2019-06-04 16:35:44 -0400

    Update June walleye fingerprint

--- abc.mk
+++ abc.mk
@@ -30,8 +30,8 @@ PRODUCT_MANUFACTURER := Google
 # Device Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=walleye \
-    BUILD_FINGERPRINT=google/walleye/walleye:9/PQ3A.190505.001/5373320:user/release-keys \
-    PRIVATE_BUILD_DESC="walleye-user 9 PQ3A.190505.001 5373320 release-keys"
+    BUILD_FINGERPRINT=google/walleye/walleye:9/PQ3A.190605.003/5524043:user/release-keys \
+    PRIVATE_BUILD_DESC="walleye-user 9 PQ3A.190605.003 5524043 release-keys"
 
 $(call inherit-product-if-exists, vendor/google/walleye/walleye-vendor.mk)
 $(call inherit-product-if-exists, vendor/pixelgapps/pixel-gapps.mk)
