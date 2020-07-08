f78b7f50c96f54b883bba6c44958e5fd3beee7e1
buffal0b1ll <billblandford@gmail.com>
2020-07-08 12:43:28 -0400

    Update build fingerprint to QQ3A.200705.002

--- abc.mk
+++ abc.mk
@@ -32,12 +32,12 @@ PRODUCT_MANUFACTURER := Google
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="walleye" \
     TARGET_DEVICE="walleye" \
-    PRIVATE_BUILD_DESC="walleye-user 10 QQ3A.200605.001 6392402 release-keys"
+    PRIVATE_BUILD_DESC="walleye-user 10 QQ3A.200705.002 6506677 release-keys"
 
-BUILD_FINGERPRINT := "google/walleye/walleye:10/QQ3A.200605.001/6392402:user/release-keys"
+BUILD_FINGERPRINT := "google/walleye/walleye:10/QQ3A.200705.002/6506677:user/release-keys"
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.fingerprint=google/walleye/walleye:10/QQ3A.200605.001/6392402:user/release-keys
+    ro.build.fingerprint=google/walleye/walleye:10/QQ3A.200705.002/6506677:user/release-keys
 
 $(call inherit-product-if-exists, vendor/google/walleye/walleye-vendor.mk)
 $(call inherit-product-if-exists, vendor/pixelgapps/pixel-gapps.mk)
